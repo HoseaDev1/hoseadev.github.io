9ec6c5d96ab8e229ea82b86942f2c782b61123bd
HoseaDev <115222776+HoseaDev1@users.noreply.github.com>
2022-12-04 15:56:29 +0700

    create index.html file

---
+++ main.js
@@ -0,0 +1,14 @@
+function skillClick() {
+    var konten = document.getElementById("skills-content");
+    konten.scrollIntoView();
+}
+
+function articlesClick() {
+    var konten = document.getElementById("articles-content");
+    konten.scrollIntoView();
+}
+
+function aboutClick() {
+    var content = document.getElementById("about-content");
+    content.scrollIntoView();
+}
